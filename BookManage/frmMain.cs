b977c3d5ba0cb86cf9c948415e6285fd89a571f7
magicalLineo <lineo12356@gmail.com>
2021-12-11 15:33:10 +0800

    update

---
+++ BookManage/frmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookManage.Model;
+
+namespace BookManage
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            InitMenu();
+        }
+
+        public void InitMenu()
+        {
+            Reader reader = frmLogin.reader;
+            图书管理ToolStripMenuItem.Enabled = 
+        }
+    }
+}
